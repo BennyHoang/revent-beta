c365efc74aeaddde9c9fa14abc5210365989cddf
BennyHoang <hoaben13@student.westerdals.no>
2015-11-16 22:38:38 +0100

    test

--- wp-content/plugins/buddypress/bp-templates/bp-legacy/buddypress/members/members-loop.php
+++ wp-content/plugins/buddypress/bp-templates/bp-legacy/buddypress/members/members-loop.php
@@ -132,7 +132,7 @@ do_action( 'bp_before_directory_members_list' ); ?>
 
 						echo $sattKategori; ?></span>
 						</div>
-						<button class="utvalg-sendforesporsel" value="send forespørsel"></button>
+						<button class="utvalg-sendforesporsel"></button>
 						<?php
 						$navnArtist = bp_get_member_name();
 						//echo $navnArtist;
